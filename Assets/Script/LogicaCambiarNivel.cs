e34bbb2f48b3391b14f805fb917d4f9284807787
ryElizabeth121 <elizabeth.rojas.40789@usalesiana.edu.bo>
2022-09-20 20:03:10 -0400

    Se agrega el proyecto

---
+++ Assets/Script/LogicaCambiarNivel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class LogicaCambiarNivel : MonoBehaviour
+{
+    public bool PasarNivel;
+    public int IndiceNivel;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CambiarNivel(IndiceNivel);
+        }
+
+        if (PasarNivel)
+        {
+            CambiarNivel(IndiceNivel);
+        }
+    }
+
+    public void CambiarNivel(int Indice)
+    {
+        SceneManager.LoadScene(Indice);
+    }
+
+}
